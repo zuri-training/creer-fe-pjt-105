eafdb9bd5fa0b60ab895393b51f061beba19bcb1
Godson <imiebo.godson@gmail.com>
2021-06-27 20:13:32 +0100

    little changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css'
 import LandingPage from './components/LandingPage/LandingPage';
 import SignIn from './components/SignIn/SignIn';
+import Homepage from './components/Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
         {/* When you are building a component/feature/page add it to here to see your changes in realtime */}
-        <LandingPage />
+        <Homepage />
     </div>
   );
 }
---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import './Homepage.css';
+
+const Homepage = () => {
+    return (
+        <div>
+            {/* Homepage goes here */}
+        </div>
+    )
+}
+
+export default Homepage;
