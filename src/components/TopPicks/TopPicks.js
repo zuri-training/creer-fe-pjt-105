4fe67db124c5af226ea45bd675c89f8b49fe5bbc
Godson <imiebo.godson@gmail.com>
2022-04-14 23:43:25 +0100

    Fix: worked on the right and left side components

--- src/components/TopPicks/TopPicks.js
+++ src/components/TopPicks/TopPicks.js
@@ -4,13 +4,13 @@ const TopPicks = () => {
     return (
         <div className='topPicks'>
             <p className='topPicksTitleText'>Top picks for you</p>
-            <ol className='topPicksList'>
+            <ul className='topPicksList'>
                 <li className='topPicksListItem'>What's the best stack for build a social platform?</li>
                 <li className='topPicksListItem'>What should i do to get my first job in tech?</li>
                 <li className='topPicksListItem'>What's the most common error in learning to code?</li>
                 <li className='topPicksListItem'>What's the React library for?</li>
                 <li className='topPicksListItem'>What's the best job in tech?</li>
-            </ol>
+            </ul>
         </div>
     )
 }
