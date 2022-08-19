67ad3832677500c0848b5b303a3b5ef8504379d9
godson imiebo <imiebo.godson@gmail.com>
2022-08-19 15:46:03 +0100

    Fixed: create a form component for the sign and sign up page

---
+++ src/components/Form/Form.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './Form.css';
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+
+const Form = ({ setAuth, formType }) => {
+
+    const isLogin = formType === 'signin';
+
+  return (
+    <div id="wrapper">
+        <div id='formContainer'>
+            <div className='welcomeTextContainer'>
+                <p className='welcomeTextBig'>{isLogin ? 'Welcome back!' : 'Welcome to Creer!'}</p>
+            </div>
+            <div className='googleSignInBtn'>
+                <p>Sign in with Google</p>
+                <FcGoogle  className='googleLogo'/>
+            </div>
+            <div className="orTextWrapper">
+                <hr/>
+                <p className="orText">OR</p>
+                <hr/>
+            </div>
+            
+            <form>
+                <div className="container">
+                    <>
+                        {
+                            isLogin ?
+                            ('') :
+                            (
+                                <div className='formNameBoxContainer'>
+                                    <div className='formItem formBox nameFormBox firstNameInputContainer'>
+                                        <input className='formBoxInput firstNameFormBoxInput formNameBoxInput' type="text" placeholder="First Name" name="name" required/>
+                                    </div>
+
+                                    <div className='formItem formBox nameFormBox lastNameInputContainer'>
+                                        <input className='formBoxInput lastNameFormBoxInput formNameBoxInput' type="text" placeholder="Last Name" name="name" required/>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </>
+                    <div className='formItem formBox emailInputContainer'>
+                        <input className='formBoxInput' type="text" placeholder="Email Address" name="uname" required/>
+                    </div>
+
+                    <div className='formItem formBox passwordInputContainer'>
+                        <input className='formBoxInput' type="password" placeholder="Password" name="psw" required/>
+                    </div>
+                        
+                    <Link onClick={() => setAuth()} className='formBtn' to={isLogin ? '/home' : '/profile'} style={{textAlign: 'center', textDecoration: 'none', fontWeight: '600', color: '#fff'}}>{isLogin ? 'Log In' : 'Sign Up'}</Link>
+                    
+                </div>
+            </form>
+        </div>
+        <div className='bottomInfoTextContainer'>
+            <p className='bottomInfoText'>{isLogin ? "Don't have an account?" : 'Got an account?'} <Link className="linkText" style={{textDecoration: 'none', fontWeight: '600', color: '#3757FF'}} to={isLogin ? '/signup' : '/signin'}>{isLogin ? 'Register' : 'Sign In'}</Link></p>
+        </div>
+    </div>
+  )
+}
+
+export default Form
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,61 +1,10 @@
 import './SignUp.css';
-import { FcGoogle } from "react-icons/fc";
-import { HiOutlineMail } from "react-icons/hi";
-import { CgLock } from "react-icons/cg";
-import { Link } from "react-router-dom";
+import Form from '../../components/Form/Form';
 
  function SignUp({ setAuth }) {
   return (
     <div className='signup'>
-        <div className='signupForm'>
-            <div className='welcomeTextContainer shortTextContainer'>
-                {/* <p className='welcomeTextBig'>Welcome back!</p> */}
-                <p className='welcomeTextSmall'>Welcome to Creer!</p>
-            </div>
-            <div className='googleSignInBtn'>
-                <p>Continue with Google</p>
-                <FcGoogle  className='googleLogo'/>
-            </div>
-            <div className='loginOrDecoWrapper'>
-                <hr/>
-                    <p className="loginOrText">OR</p>
-                <hr/>
-            </div>
-            <form>
-                <div className="container">
-                    <label className='formItem formLabel' htmlFor="name">Name *</label>
-                    <div className='formNameBoxContainer'>
-                        <div className='formItem formBox signupFormBox firstNameInputContainer'>
-                            <input className='formBoxInput firstNameFormBoxInput formNameBoxInput' type="text" placeholder="First Name" name="name" required/>
-                        </div>
-
-                        <div className='formItem formBox signupFormBox lastNameInputContainer'>
-                            <input className='formBoxInput formNameBoxInput' type="text" placeholder="Last Name" name="name" required/>
-                        </div>
-                    </div>
-                    
-
-                    <label className='formItem formLabel' htmlFor="email">Email *</label>
-                    <div className='formItem formBox'>
-                        <HiOutlineMail className='formIcons'/>
-                        <input className='formBoxInput' type="text" placeholder="Enter your Email Address" name="email" required/>
-                    </div>
-
-                    <label className='formItem formLabel' htmlFor="psw">Password *</label>
-                    <div className='formItem formBox passwordInputContainer'>
-                        <CgLock className='formIcons'/>
-                        <input className='formBoxInput' type="password" placeholder="Enter your password" name="psw" required/>
-                    </div>
-                        
-                    {/* <button className='signupBtn' type="submit">Get Started</button> */}
-                    <Link onClick={() => setAuth()} className='signupBtn' to='/profile' style={{textAlign: 'center', textDecoration: 'none', fontWeight: '600', color: '#fff'}}>Get Started</Link>
-                    
-                </div>
-            </form>
-            <div className='bottomInfoTextContainer'>
-                <p className='bottomInfoText'>Already have an account? <Link to={'/signin'} style={{textDecoration: 'none', fontWeight: '600', color: '#3757FF'}}>Log In</Link></p>
-            </div>
-        </div>
+        <Form setAuth={setAuth} formType={'signup'} />
     </div>
   );
 }
