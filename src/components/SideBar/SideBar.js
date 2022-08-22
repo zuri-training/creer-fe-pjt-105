85a3de99279ab8707e2f8e43cbbd2ec7fde3f4b6
godson imiebo <imiebo.godson@gmail.com>
2022-08-22 12:15:16 +0100

    added styling fixes to sidebar component

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -5,27 +5,25 @@ import { FiHash  } from 'react-icons/fi';
 import { BsPeople  } from 'react-icons/bs';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import CreateASpace from '../CreateASpace/createASpace';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 
 const SideBar = () => {
 
-    const [isClicked, setIsClicked] = useState(false);
+    // const [isClicked, setIsClicked] = useState(false);
 
     const yourCommunityListToggle = () => {
+        const community = document.getElementById('yourCommunities');
         const communityList = document.getElementById('yourCommunityList');
         communityList.classList.toggle('noDisplay');
+        community.classList.toggle('onDropdown');
     }
 
     const yourTopicListToggle = () => {
-        if(!isClicked) {
-            setIsClicked(true)
-        } else {
-            setIsClicked(false)
-        }
-
-        // const topicsList = document.getElementById('yourTopicsList');
-        // topicsList.classList.toggle('noDisplay');
+        const topics = document.getElementById('yourTopics');
+        const topicsList = document.getElementById('yourTopicsList');
+        topicsList.classList.toggle('noDisplay');
+        topics.classList.toggle('onDropdown');
     }
 
     return (
@@ -34,7 +32,7 @@ const SideBar = () => {
             <ul className='mainSideBarOptions'>
                 <li className='home'><AiOutlineHome  className='sidebarIcons'/><p>Home</p></li>
                 <li className='savedPosts'><FaRegBookmark className='sidebarIcons' /><p>Saved posts</p></li>
-                <li className='yourTopics' onClick={yourTopicListToggle}><FiHash className='sidebarIcons' /><p>Topics</p><MdKeyboardArrowDown className='dropdownIcon'/></li>
+                <li id='yourTopics' className='yourTopics' onClick={yourTopicListToggle}><FiHash className='sidebarIcons' /><p>Topics</p><MdKeyboardArrowDown className='dropdownIcon'/></li>
                     <ul className='yourTopicsList noDisplay' id='yourTopicsList'>
                         <li>
                             <img alt="community" src={process.env.PUBLIC_URL + '/assets/communityImage1.png'} className='sideBarTopicsImage'/>
@@ -53,8 +51,8 @@ const SideBar = () => {
                             <p>Starting Out In Tech</p>
                         </li> 
                     </ul>
-                <li className={`${isClicked ? '' : 'yourCommunities'}`} onClick={yourCommunityListToggle}><BsPeople className='sidebarIcons'/><p>My Communities</p><MdKeyboardArrowDown className='dropdownIcon'/></li>
-                    <ul className={`yourCommunityList ${isClicked ? '' : 'noDisplay'}`} id='yourCommunityList'>
+                <li id='yourCommunities' className='yourCommunities' onClick={yourCommunityListToggle}><BsPeople className='sidebarIcons'/><p>My Communities</p><MdKeyboardArrowDown className='dropdownIcon'/></li>
+                    <ul className='yourCommunityList noDisplay' id='yourCommunityList'>
                         <li>
                             <img alt="community" src={process.env.PUBLIC_URL + '/assets/communityImage1.png'} className='sideBarCommunityImage'/>
                             <p>Frontend Developers</p>
