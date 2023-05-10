313256ea71624fcb60d3f114870e256cefbb2ab0
Godson Imiebo <imiebo.godson@gmail.com>
2023-05-10 23:41:08 +0100

    fix: updated packages and fixed console errors

--- src/components/Faqs/Faqs.js
+++ src/components/Faqs/Faqs.js
@@ -1,48 +1,43 @@
-import { useState } from 'react';
-import './Faqs.css';
-import { FAQs } from '../../db';
-import { GrAdd } from 'react-icons/gr';
+import { useState } from "react";
+import "./Faqs.css";
+import { FAQs } from "../../db";
+import { GrAdd } from "react-icons/gr";
 import { FiMinus } from "react-icons/fi";
 
 const Faqs = () => {
-
-const FaqItem = ({ faqData }) => {
-
+  const FaqItem = ({ faqData }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const onBtnClick = () => {
-        if(!isOpen) {
-            setIsOpen(true)
-        } else {
-            setIsOpen(false)
-        }
-    }
-
-    
+      if (!isOpen) {
+        setIsOpen(true);
+      } else {
+        setIsOpen(false);
+      }
+    };
 
     return (
-        <div className='faqsQuestionAndAnswer'>
-            <div className='faqsQuestion' onClick={() => onBtnClick()}>
-                <p>{faqData.question}</p>
-                <button className='FaqBtn'>{!isOpen ? <GrAdd /> : <FiMinus />}</button>
-            </div>
-            <div className={`faqsAnswer ${isOpen ? 'open' : 'closed'}`}>
-                <p>{faqData.answer}</p>
-            </div>
+      <div className="faqsQuestionAndAnswer">
+        <div className="faqsQuestion" onClick={() => onBtnClick()}>
+          <p>{faqData.question}</p>
+          <button className="FaqBtn">
+            {!isOpen ? <GrAdd /> : <FiMinus />}
+          </button>
         </div>
-    )
-}
-
+        <div className={`faqsAnswer ${isOpen ? "open" : "closed"}`}>
+          <p>{faqData.answer}</p>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <>
-        {
-            FAQs.map((faq) => {
-                return <FaqItem faqData={faq} />
-            })
-        }
+      {FAQs.map((faq) => {
+        return <FaqItem key={faq.id} faqData={faq} />;
+      })}
     </>
-  )
-}
+  );
+};
 
-export default Faqs
+export default Faqs;
